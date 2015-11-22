189604852e12ca7a7e1d114c3caa59663f430994
Jon Higgins <jon@jonhiggins.co.uk>
2015-11-22 20:39:42 +0000

    arrowDownButton: detect autoscroll and don't hide

--- src/modules/arrowDownButton/arrowDownButton.js
+++ src/modules/arrowDownButton/arrowDownButton.js
@@ -12,8 +12,7 @@ var options = {
 
 var $button = $('#arrowDownButton'),
     $window = $(window),
-    $currentSection = $('.section').eq(0), //@TODO change depending on scroll position
-    $nextSection;
+    $currentSection = $('.section').eq(0); //@TODO change depending on scroll position
 
 /**
  * @constructor ArrowDownButton
@@ -22,114 +21,153 @@ var $button = $('#arrowDownButton'),
 
 var ArrowDownButton = module.exports = function(controller) {
     'use strict';
-    changeIconColour();
-    setInitialHash();
-    $button.on('click', buttonClick);
-    $window.one('scroll', pageScroll);
 
-    console.log(controller.props.property);
-    controller.props.property = 'aaa';
-    console.warn(controller.props.property);
-};
+    /**
+     * @function init
+     */
 
-/**
- * Set hash/href of button to next section
- * @function setInitialHash
- */
+    var init = function() {
+        changeIconColour();
+        setInitialHash();
+        $button.on('click', buttonClick);
+        $window.on('scroll', pageScroll); // @TODO debounce or pub/sub
+    };
 
-var setInitialHash = function() {
-	// @TODO $nextSection selector simplify. accounts for scrollmagic pin
-    var $nextSection = $currentSection.next().hasClass('.section') ? $currentSection.next() : $currentSection.next().find('.section'),
-		nextSectionId = $nextSection.attr('id');
 
-	if (nextSectionId) {
-		$button.prop('hash', nextSectionId);
-	} else {
-		$window.on('sections:sectionsInited', setInitialHash);
-	}
 
-};
+    /**
+     * Set hash/href of button to next section
+     * @function setInitialHash
+     */
 
-/**
- * On clicking the arrow button
- * @function buttonClick
- */
+    var setInitialHash = function() {
+        var $nextSection = getNextSection($currentSection),
+            nextSectionId = $nextSection.attr('id');
 
-var buttonClick = function(e) {
+    	if (nextSectionId) {
+    		$button.prop('hash', nextSectionId);
+    	} else {
+    		$window.on('sections:sectionsInited', setInitialHash);
+    	}
+    };
 
-    e.preventDefault();
+    /**
+     * Get next section. @TODO should this be in section module?
+     * @function getNextSection
+     * @param {jQuery} $currentSection
+     * @returns {jQuery} $nextSection
+     */
 
-    var hash = $(this).prop('hash'),
-        sectionTop = $(hash).offset().top;
+    var getNextSection = function($currentSection) {
+    	// Accounts for scrollmagic pin's extra div
+        var $nextSection;
 
-    // Scroll to next section top
-    TweenLite.to(window, options.scrollDownDuration, {
-        scrollTo: {
-            y: sectionTop
-        },
-        ease: Power2.easeOut,
-        onComplete: scrollComplete
-    });
+        if ($currentSection.next('.section').length) {
+            $nextSection = $currentSection.next();
+        } else {
+            $nextSection = $currentSection.next().find('.section');
+        }
 
-    // Hide arrow
-    buttonHide();
-};
 
-/**
- * When page scrolling is complete: show button and update hash
- * @function scrollComplete
- */
+    	return $nextSection;
+    };
 
-var scrollComplete = function() {
+    /**
+     * On clicking the arrow button
+     * @function buttonClick
+     */
 
-    // Update button's hash to next section
-    var hash = $button.prop('hash');
+    var buttonClick = function(e) {
 
-    $currentSection = $(hash);
-    $nextSection = $currentSection.next();
+        e.preventDefault();
 
-    if ($nextSection.length) {
-        $button.prop('hash', '#' + $nextSection.prop('id'));
-        buttonShow();
-    }
+        var hash = $(this).prop('hash'),
+            sectionTop = $(hash).offset().top;
 
-};
+        // Update controller state
+        controller.props.autoScrolling = true;
 
-/**
- * @function buttonHide
- */
+        // Scroll to next section top
+        TweenLite.to(window, options.scrollDownDuration, {
+            scrollTo: {
+                y: sectionTop
+            },
+            ease: Power2.easeOut,
+            onComplete: scrollComplete
+        });
+    };
 
-var buttonHide = function() {
-    $button.addClass('hidden');
-};
+    /**
+     * When page scrolling is complete: show button and update hash
+     * @function scrollComplete
+     */
 
-/**
- * @function buttonShow
- */
+    var scrollComplete = function() {
 
-var buttonShow = function() {
-    changeIconColour();
-    $button.removeClass('hidden');
-};
+        var hash,
+            $nextSection;
 
-/**
- * Change icon colour to stand out from background
- * @function changeIconColour
- */
+        // Update controller state. Seems to need timeout for extra scrolls after scrollComplete event
+        window.setTimeout(function() {
+            controller.props.autoScrolling = false;
+        }, 300);
 
-var changeIconColour = function() {
-    if ($currentSection.attr('data-background') === 'white') {
-        $button.attr('data-colour', 'black');
-    } else {
-        $button.attr('data-colour', 'white');
-    }
-};
+        // Update button's hash to next section
+        hash = $button.prop('hash');
+        $currentSection = $(hash);
+        $nextSection = getNextSection($currentSection);
 
-/**
- * Hide the arrow when scrolling normally
- * @function pageScroll
- */
+        if ($nextSection.length) {
+            $button.prop('hash', '#' + $nextSection.prop('id'));
+            buttonShow();
+        }
 
-var pageScroll = function() {
-    buttonHide();
+    };
+
+    /**
+     * @function buttonHide
+     */
+
+    var buttonHide = function() {
+        $button.addClass('hidden');
+    };
+
+    /**
+     * @function buttonShow
+     */
+
+    var buttonShow = function() {
+        changeIconColour();
+        $button.removeClass('hidden');
+    };
+
+    /**
+     * Change icon colour to stand out from background
+     * @function changeIconColour
+     */
+
+    var changeIconColour = function() {
+        if ($currentSection.attr('data-background') === 'white') {
+            $button.attr('data-colour', 'black');
+        } else {
+            $button.attr('data-colour', 'white');
+        }
+    };
+
+    /**
+     * Hide the arrow when scrolling normally
+     * @function pageScroll
+     */
+
+    var pageScroll = function(e) {
+        // Only hide button when user manually scrolls
+        if (!controller.props.autoScrolling) {
+            $window.off('scroll', pageScroll);
+            buttonHide();
+        }
+
+    };
+
+    init();
+
 };
--- src/modules/controller/controller.js
+++ src/modules/controller/controller.js
@@ -7,8 +7,14 @@
 var controller = module.exports = function() {
   'use strict';
 
+  /**
+   * App properties, states and settings
+   * @namespace $prop
+   * @property {boolean} autoScrolling is app auto-scrolling? Used to differentiate manual scrolling
+   */
+
   this.props = {
-      
+      autoScrolling: false
   };
 
   return this;
