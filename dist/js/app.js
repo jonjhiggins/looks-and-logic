177f9a92d508a25c527de39bc53e820154b05e12
Jon Higgins <jon@jonhiggins.co.uk>
2015-11-28 17:29:11 +0000

    Move curiousPlayfulInformative up so that it can be seen in previous section

--- dist/js/app.js
+++ dist/js/app.js
@@ -23500,6 +23500,8 @@ var sectionMakingDigitalHuman = module.exports = function(controller, $section, 
     var sectionObject = controller.props.sections[index],
         scene = sectionObject.props.scene;
 
+    // Set triggerHook to top of component (so it pins there)
+    scene.triggerHook(0);
     scene.setPin($section.get(0), {
         pushFollowers: false
     });
