c2cc0009e1cb31fbdd93a087d4c74aa4672737d2
Jon Higgins <jon@jonhiggins.co.uk>
2015-11-29 13:58:16 +0000

    sectionIntro: change ball position on window resize

--- src/modules/sectionIntro/sectionIntro.js
+++ src/modules/sectionIntro/sectionIntro.js
@@ -29,22 +29,25 @@ var SectionIntro = module.exports = function(controller, $element) {
     };
 
     /**
+     * logo svgObject created by snap.svg
+     * @var {object} svgObject
+     */
+
+    var svgObject = null;
+
+    /**
      * Initialise the component
      * Everything here should be undone using the "reset" function
      * @function init
      */
 
     var init = function() {
-        controller.emitter.on('section:sectionLeave', function($sectionLeave) {
-            // When leaving this section, trigger ball1Drop
-            if ($sectionLeave.get(0) === $element.get(0)) {
-                controller.emitter.emit('balls:ball1Drop');
-            }
-        });
-
+        // On leave: drop ball
+        controller.emitter.on('section:sectionLeave', sectionLeave);
+        // Refresh dimensions on resize
+        controller.emitter.on('window:resize', measureAndShowBalls);
+        // Load the SVG
         loadSVG();
-
-
     };
 
     /**
@@ -53,23 +56,51 @@ var SectionIntro = module.exports = function(controller, $element) {
      */
 
     var loadSVG = function() {
-
-        var svgObject = snap(cache.$logoSvg.get(0));
+        svgObject = snap(cache.$logoSvg.get(0));
         snap.load('../img/logo.svg', function(loadedSVG) {
             // Add SVG
             svgObject.append(loadedSVG);
 
-            // Measure balls within SVGs
-            var ball1Position = svgObject.select('#ball1').node.getBoundingClientRect(),
-                ball2Position = svgObject.select('#ball2').node.getBoundingClientRect();
-
-            // Hide background image
-            cache.$logo.addClass('section__logo--with-svg');
-
-            //@TODO promise
-            controller.emitter.emit('balls:showBall1', ball1Position);
-            controller.emitter.emit('balls:showBall2', ball2Position);
+            measureAndShowBalls();
         });
+    };
+
+    /**
+     * Reset all component behaviour, remove handlers
+     * @function reset
+     */
+
+    var measureAndShowBalls = function() {
+        // Only run after snap.svg has done it's stuff
+        if (!svgObject) {
+            return;
+        }
+
+        // Temporarily show balls in background image for measuring
+        cache.$logo.removeClass('section__logo--with-svg');
+        // Measure balls within SVGs
+        var ball1Position = svgObject.select('#ball1').node.getBoundingClientRect(),
+            ball2Position = svgObject.select('#ball2').node.getBoundingClientRect();
+        // Hide background image
+        cache.$logo.addClass('section__logo--with-svg');
+
+        //@TODO promise
+        controller.emitter.emit('balls:showBall1', ball1Position);
+        controller.emitter.emit('balls:showBall2', ball2Position);
+    };
+
+    /**
+     * On leaving component: drop ball and stop listening for resizes
+     * @function sectionLeave
+     * @param {jquery} $sectionLeave
+     */
+
+    var sectionLeave = function($sectionLeave) {
+        // When leaving this section, trigger ball1Drop
+        if ($sectionLeave.get(0) === $element.get(0)) {
+            controller.emitter.emit('balls:ball1Drop');
+            controller.emitter.removeListener('window:resize', measureAndShowBalls);
+        }
     };
 
     /**
