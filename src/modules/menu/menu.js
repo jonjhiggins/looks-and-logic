a8e5ca84ae1576a3302f5b82d908432d227b07ae
Jon Higgins <jon@jonhiggins.co.uk>
2016-01-12 15:55:33 +0000

    menu: add class to body when open

--- src/modules/menu/menu.js
+++ src/modules/menu/menu.js
@@ -10,7 +10,51 @@ var $ = require('jquery');
 var Menu = module.exports = function(controller) {
   'use strict';
 
-  var $body = $('body'),
-      $button = $('#menu__button');
+  /**
+   * jQuery elements
+   * @namespace cache
+   * @property {jQuery} window
+   */
+
+  var cache = {
+      $menuButton: $('#menu__button'),
+      $body: $('body')
+  };
+
+  /**
+   * Module properties, states and settings
+   * @namespace $prop
+   * @property {boolean} open is menu active
+   */
+
+  var props = {
+      open: false
+  };
+
+  /**
+   * Initialise the component
+   * @method init
+   */
+
+  this.init = function() {
+      cache.$menuButton.on('click', buttonClick);
+  };
+
+  /**
+   * @function buttonClick
+   */
+
+  var buttonClick = function() {
+
+      if (!props.open) {
+          cache.$body.addClass('js--menuOpen');
+      } else {
+          cache.$body.removeClass('js--menuOpen');
+      }
+
+      props.open = !props.open;
+  };
+
+  this.init();
 
 };
